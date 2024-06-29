0a38f88da31a0a58fb98bfb88955bac3f5945d76
mohamed-younes16 <bettiouim77@gmail.com>
2024-06-29 22:54:57 +0100

    first commit

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,16 +1,16 @@
-// import getCurrentUser from "@/actions";
-// import Heading from "@/components/Heading";
-// import SubscribeModal from "@/components/SubscribeModal";
-// import { Separator } from "@/components/ui/separator";
-// import { getUserSubscriptionPlan } from "@/lib/subscription";
-// import CliComp from "@/providers/modalProvider";
-// import { CheckCircle, Settings } from "lucide-react";
+"use client";
+import Heading from "@/components/Heading";
+import SubscribeModal from "@/components/SubscribeModal";
+import { Separator } from "@/components/ui/separator";
+import { useGetPlan } from "@/hooks/purchase-hooks";
+import CliComp from "@/providers/modalProvider";
+import { CheckCircle, Loader2, Settings } from "lucide-react";
 
-const page =  () => {
-  // const { isPro } = await getUserSubscriptionPlan();
+const page = () => {
+  const { data: plan } = useGetPlan();
   return (
     <div className="bg-background min-h-[40dvh] rounded-md py-4  w-full px-8 max-lg:px-4 ">
-      {/* <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between">
         <Heading
           icon={<Settings className="text-foreground h-10 w-10" />}
           title={`Your settings Options `}
@@ -22,22 +22,28 @@ const page =  () => {
         className="flex p-3 rounded-xl shadow-sm hover:shadow-xl 
       transition-all border-foreground/20 border-[1px]  items-center justify-between"
       >
-        <p className=" text-lg font-semibold">Subpscription</p>
-        <div>{isPro ? "You are On A Pro Plan" : "No Plan Yet"}</div>
-        <CliComp>
-          <div>
-            {" "}
-            {!isPro ? (
-              <SubscribeModal />
-            ) : (
-              <div className=" flexcenter font-semibold text-minor gap-4">
-                <CheckCircle strokeWidth={3} className="h-8 w-8" />
-                <p>Subscribed</p>
+        {plan ? (
+          <>
+            <p className=" text-lg font-semibold">Subpscription</p>
+            <div>{plan?.isPro ? "You are On A Pro Plan" : "No Plan Yet"}</div>
+            <CliComp>
+              <div>
+                {" "}
+                {!plan.isPro ? (
+                  <SubscribeModal />
+                ) : (
+                  <div className=" flexcenter font-semibold text-minor gap-4">
+                    <CheckCircle strokeWidth={3} className="h-8 w-8" />
+                    <p>Subscribed</p>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-        </CliComp>
-      </div> */}
+            </CliComp>
+          </>
+        ) : (
+          <Loader2 className=" h-8 w-8 text-foreground animate-spin" />
+        )}
+      </div>
     </div>
   );
 };
