08431d9053a49b55e906b477343c4521cf3470dc
Younes <bettiouim77@gmail.com>
2025-09-11 17:07:51 +0100

    fixed auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -59,22 +59,22 @@ export const authOpts: AuthOptions = {
   session: {
     strategy: "jwt",
   },
-  cookies: {
-    sessionToken: {
-      name:
-        process.env.NODE_ENV === "production"
-          ? `__Secure-next-auth.session-token`
-          : "next-auth.session-token",
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-        domain:
-          process.env.NODE_ENV === "production" ? ".vercel.app" : undefined,
-      },
-    },
-  },
+  // cookies: {
+  //   sessionToken: {
+  //     name:
+  //       process.env.NODE_ENV === "production"
+  //         ? `__Secure-next-auth.session-token`
+  //         : "next-auth.session-token",
+  //     options: {
+  //       httpOnly: true,
+  //       sameSite: "lax",
+  //       path: "/",
+  //       secure: process.env.NODE_ENV === "production",
+  //       domain:
+  //         process.env.NODE_ENV === "production" ? ".vercel.app" : undefined,
+  //     },
+  //   },
+  // },
 };
 export const handler = NextAuth(authOpts);
 export { handler as GET, handler as POST };
